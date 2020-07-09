e2e9b8d60d4f36152ea8675893f6995f2f7ce19d
Italo M <italomarcos0010@gmail.com>
2020-07-09 08:41:45 -0300

    a

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,36 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
-// import { store } from '~/store';
-
-export default function RouteWrapper({
-  component: Component,
-  isPrivate,
-  ...rest
-}) {
-  const signed = true;
-
-  if (!signed && isPrivate) {
-    return <Redirect to="/" />;
-  }
-
-  if (signed && !isPrivate) {
-    return <Redirect to="/dashboard" />;
-  }
-
-  const Layout = signed ? DefaultLayout : AuthLayout;
-
+export default function RouteWrapper({ component: Component, ...rest }) {
   return (
     <Route
       {...rest}
       render={props => (
-        <Layout>
+        <DefaultLayout>
           <Component {...props} />
-        </Layout>
+        </DefaultLayout>
       )}
     />
   );
