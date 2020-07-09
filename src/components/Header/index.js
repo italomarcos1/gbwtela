25ca2172d1e8c2de841105d6f77822ce1d51b496
Italo M <italomarcos0010@gmail.com>
2020-07-09 08:47:37 -0300

    b

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ export default function Header() {
       <Content>
         <nav>
           <img src={logo} alt="GoBarber" />
-          <Link to="/dashboard">DASHBOARD</Link>
+          <Link to="/">DASHBOARD</Link>
         </nav>
         <aside>
           <Notifications />
