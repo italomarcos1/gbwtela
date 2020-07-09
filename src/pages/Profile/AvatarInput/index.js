1b0e1516dfe79698a1fe295369d4f8069559ff2a
Italo M <italomarcos0010@gmail.com>
2020-07-09 08:31:10 -0300

    a

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -1,40 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useField } from '@rocketseat/unform';
-import api from '~/services/api';
+import React, { useRef } from 'react';
 
 import { Container } from './styles';
 
 export default function AvatarInput() {
-  const { defaultValue, registerField } = useField('avatar'); // puxa o campo do redux
-
-  const [file, setFile] = useState(defaultValue && defaultValue.id);
-  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+  const file = 63;
+  const preview = 'https://api.adorable.io/avatars/120/abott@adorable.png';
 
   const ref = useRef();
-
-  useEffect(() => {
-    // permite o unform conseguir acessar a informação do input
-    if (ref.current) {
-      registerField({
-        name: 'avatar_id', // valor do id
-        ref: ref.current, // nome do input
-        path: 'dataset.file', // puxar o data-file
-      });
-    }
-  }, [ref, registerField]);
-
-  async function handleChange(e) {
-    const data = new FormData(); // formato do multer
-
-    data.append('file', e.target.files[0]); // nome usado no insomnia. retorna array mas usamos single element
-
-    const response = await api.post('files', data); // envia pra api
-
-    const { id, url } = response.data;
-
-    setFile(id);
-    setPreview(url);
-  }
 
   return (
     <Container>
